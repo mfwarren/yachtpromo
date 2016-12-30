4b1b59209d35b66ebab2cd0ae443392290348fca
Matt Warren <matt.warren@gmail.com>
2016-12-30 11:40:12 -0500

    added an static image type slide for the beginning

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ import json
 import os
 import sys
 
-from slides import KenBurnsSlide
+from slides import KenBurnsSlide, IntroSlide
 from render import Renderer
 
 
 def make_video(boat_info, pictures):
     print(boat_info['name'], len(pictures))
     slides = deque()
+    slides.append(IntroSlide(pictures[0]))
     for picture in pictures:
-        slides.append(KenBurnsSlide(picture, 'testing', 1, (0,0), 1.4, (500,300)))
+        slides.append(KenBurnsSlide(picture, 1, (0,0), 1.4, (500,300)))
 
     Renderer(slides, f'{boat_info["name"]}.mp4')
 
--- slides.py
+++ slides.py
@@ -13,50 +13,12 @@ NUM_FRAMES = SLIDE_FRAMES + (TRANSITION_FRAMES * 2)
 
 FONT = ImageFont.truetype('./fonts/MerriweatherSans-Regular.ttf', 55)
 
-
-class Slide(object):
-    def __init__(self, filename, caption=None):
+class IntroSlide(object):
+    """full size image to fit video resolution"""
+    def __init__(self, filename):
         self.filename = filename
-        self.caption = caption
-        self.num_frames = NUM_FRAMES
-        self.slide_frames = SLIDE_FRAMES
-
-    def load(self):
-        self.original = Image.open(self.filename)
-        # self.original.save('orig.jpg', 'jpeg')
-        self.render_caption()
-
-    def render_caption(self):
-        if self.caption:
-            self.overlay = Image.new('RGBA', RESOLUTION, (0,0,0,0))
-            d = ImageDraw.Draw(self.overlay)
-
-            text_size = d.textsize(self.caption, font=FONT)
-            margin = (100,20)
-            padding = (16, 12)
-            text_position = (margin[0]+padding[0], RESOLUTION[1]-margin[1]-padding[1]-text_size[1])
-            rectangle = [(margin[0], RESOLUTION[1]-margin[1]-text_size[1]-2*padding[1]), (margin[0]+2*padding[0]+text_size[0], RESOLUTION[1]-margin[1])]
-
-            color = '#fff'
-            bcolor = (0, 0, 0, 140)
-
-            d.rectangle(rectangle, fill=bcolor)
-            d.text(text_position, self.caption, font=FONT, fill=color)
-        else:
-            self.overlay = None
-
-    def draw_caption(self, im):
-        if self.overlay:
-            im.paste(self.overlay, (0, 0), self.overlay)
-
-
-class KenBurnsSlide(Slide):
-    def __init__(self, filename, caption, start_scale, start_position, end_scale, end_position):
-        super().__init__(filename, caption)
-        self.start_scale = start_scale
-        self.start_position = start_position
-        self.end_scale = end_scale
-        self.end_position = end_position
+        self.num_frames = 1
+        self.slide_frames = 1
 
     @property
     def image_width(self):
@@ -67,21 +29,49 @@ class KenBurnsSlide(Slide):
         return self.original.size[1]
 
     def load(self):
-        super().load()
+        self.original = Image.open(self.filename)
 
+        # crop the image if necessary to cover full screen
         if (self.image_width/self.image_height) > (RESOLUTION[0]/RESOLUTION[1]):
-            width = self.image_width
-            height = int(self.image_width * RESOLUTION[1] / RESOLUTION[0])
-        else:
             height = self.image_height
             width = int(self.image_height * RESOLUTION[0] / RESOLUTION[1])
+        else:
+            width = self.image_width
+            height = int(self.image_width * RESOLUTION[1] / RESOLUTION[0])
 
-        if self.original.size != (width, height):
-            img = Image.new('RGB', (width, height))
-            x_offset = int((width - self.image_width) / 2)
-            y_offset = int((height - self.image_height) / 2)
-            img.paste(self.original, (x_offset, y_offset))
-            self.original = img
+        self.original = self.original.transform(RESOLUTION, Image.EXTENT, (0, 0, width, height), Image.BICUBIC)
+
+    def frame(self, n):
+        return self.original
+
+class KenBurnsSlide(IntroSlide):
+    def __init__(self, filename, start_scale, start_position, end_scale, end_position):
+        self.filename = filename
+        self.num_frames = NUM_FRAMES
+        self.slide_frames = SLIDE_FRAMES
+        self.start_scale = start_scale
+        self.start_position = start_position
+        self.end_scale = end_scale
+        self.end_position = end_position
+
+
+    def load(self):
+        self.original = Image.open(self.filename)
+
+        # if (self.image_width/self.image_height) > (RESOLUTION[0]/RESOLUTION[1]):
+        #     width = self.image_width
+        #     height = int(self.image_width * RESOLUTION[1] / RESOLUTION[0])
+        # else:
+        #     height = self.image_height
+        #     width = int(self.image_height * RESOLUTION[0] / RESOLUTION[1])
+
+        # if self.original.size != (width, height):
+        #     img = Image.new('RGB', (width, height))
+        #     x_offset = int((width - self.image_width) / 2)
+        #     y_offset = int((height - self.image_height) / 2)
+        #     img.paste(self.original, (x_offset, y_offset))
+        #     self.original = img
+        #     img.save('resized.jpg', 'jpeg')
 
     def frame(self, n):
         progress = n / self.num_frames
@@ -93,18 +83,9 @@ class KenBurnsSlide(Slide):
 
         width = self.image_width * scale
         height = self.image_height * scale
-        center_x = self.image_width + pos[0]
-        center_y = self.image_height + pos[1]
+        center_x = self.image_width /2 # * pos[0]
+        center_y = self.image_height #+ pos[1]
         # print(scale)
 
         img = self.original.transform(RESOLUTION, Image.EXTENT, (center_x - width/2, center_y - height/2, center_x + width/2, center_y + height/2), Image.BICUBIC)
-        # self.draw_caption(img)
         return img
-
-
-class BlackSlide(object):
-    def __init__(self):
-        self.original = Image.new('RGB', RESOLUTION)
-
-    def frame(self, n):
-        return self.original
